aeae515db2c36b140ab2edd7e08bb9c70e1f68b9
lexaltd <lexaltd@bigmir.net>
2020-03-31 12:11:27 +0300

    Урок 18

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import './App.css';
 import Header from './componets/Header/Header';
 import Navbar from './componets/Navbar/Navbar';
 import Profile from './componets/Profile/Profile';
+import Dialogs from "./componets/Dialogs/Dialogs";
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+	return (
+		<div className='app-wrapper'>
+			<Header/>
+			<Navbar/>
+			{/*<Profile />*/}
+			<div className='app-wrapper-content'>
+				<Dialogs/>
+			</div>
+		</div>
+	);
 };
 
 
--- src/componets/Profile/Profile.jsx
+++ src/componets/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' />
             </div>
@@ -17,6 +17,6 @@ const Profile = () => {
             </div> */}
         </div>
     );
-}
+};
 
 export default Profile;
